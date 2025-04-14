2c68fa0e9fc9559dc5e9e3c7a7c778254d762a22
jcarlosanast <jcarlos.anast@gmail.com>
2025-04-14 13:25:04 -0300

    in progress

--- src/test/java/Mockito.java
+++ src/test/java/Mockito.java
@@ -1,10 +1,13 @@
-import org.junit.Test;
+import org.junit.*;
+
+import static org.mockito.Mockito.*;
 
 public class Mockito {
 
     @Test
-    public void Test(){
-        Integer result = 200;
-        System.out.println(result);
+    public void validate_cpf_success() {
+        customerRegister = mock(CustomerRegister.class);
+        when (customerRegister.validateRealCpf(anyString())).thenReturn(true);
+        Assert.assertTrue(customerRegister.validateRealCpf("8888"));
     }
 }
--- src/test/java/Wiremock.java
+++ src/test/java/Wiremock.java
@@ -1,6 +1,8 @@
+import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.Test;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -8,4 +10,28 @@ import java.net.http.HttpResponse;
 
 public class Wiremock {
 
+    @Test
+    public void exampleTest() {
+        // Setup the WireMock mapping stub for the test
+        stubFor(post("/my/resource")
+                .withHeader("Content-Type", containing("xml"))
+                .willReturn(ok()
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("<response>SUCCESS</response>")));
+
+        // Setup HTTP POST request (with HTTP Client embedded in Java 11+)
+        final HttpClient client = HttpClient.newBuilder().build();
+        final HttpRequest request = HttpRequest.newBuilder()
+                .uri(wiremockServer.url("/my/resource"))
+                .header("Content-Type", "text/xml")
+                .POST().build();
+
+        // Send the request and receive the response
+        final HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Verify the response (with AssertJ)
+        assertThat(response.statusCode()).as("Wrong response status code").isEqualTo(200);
+        assertThat(response.body()).as("Wrong response body").contains("<response>SUCCESS</response>");
+    }
 }
