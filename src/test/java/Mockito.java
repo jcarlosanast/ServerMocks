cbcd1765050b9a8f4524a94be7c4f917951e59bc
jcarlosanast <jcarlos.anast@gmail.com>
2025-04-14 12:38:58 -0300

    estudo tipos mock

---
+++ src/test/java/Mockito.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class Mockito {
+
+    @Test
+    public void Test(){
+        Integer result = 200;
+        System.out.println(result);
+    }
+}
---
+++ src/test/java/Wiremock.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Wiremock {
+
+}
